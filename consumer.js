956a8dcec4ca00dc4d4b1a1a7794e7ffd09cbd7f
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2024-04-07 00:13:55 +0530

    #NoCard [Krish] Add consumer.js

---
+++ consumer.js
@@ -0,0 +1,35 @@
+const {kafka} = require('./client')
+
+async function init() {
+  const consumer = kafka.consumer({groupId: 'kolkata-WB'});
+
+  console.log(
+      "******************** Consumer connection: IN PROGRESS *********************");
+  await consumer.connect();
+  console.log(
+      "******************** Consumer connection: SUCCESS *********************");
+
+  await consumer.subscribe({
+    topic: "rider-updates",
+    fromBeginning: true
+  })
+
+  await consumer.run({
+    eachMessage: async ({topic, partition, message, heartbeat, pause}) => {
+      console.log(
+          `TOPIC: ${topic}, 
+           PARTITION: ${partition}`,
+           message.value.toString()
+      )
+    }
+  })
+
+  // console.log(
+  //     "******************** Consumer dis-connection: IN PROGRESS *********************");
+  // await consumer.disconnect();
+  // console.log(
+  //     "******************** Consumer dis-connection: SUCCESS *********************");
+
+}
+
+init();
